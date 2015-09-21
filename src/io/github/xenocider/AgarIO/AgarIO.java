87b47b0a8c355e9bdb95c41be63326ff96a33f69
Xenocider <xenocider.dev@gmail.com>
2015-09-21 09:45:40 -0700

    set-up intellij and created basic main java class

---
+++ src/io/github/xenocider/AgarIO/AgarIO.java
@@ -0,0 +1,13 @@
+package io.github.xenocider.AgarIO;
+
+public class AgarIO {
+
+    public static void main(String[] args) {
+        startGame();
+    }
+
+    private static void startGame() {
+        
+    }
+
+}
