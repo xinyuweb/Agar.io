c50d83b8a918f1eebbde43cca61698efa28ec3ff
Xenocider <xenocider.dev@gmail.com>
2015-09-28 13:15:24 -0700

    not working

--- src/io/github/xenocider/AgarIO/AgarIO.java
+++ src/io/github/xenocider/AgarIO/AgarIO.java
@@ -3,6 +3,7 @@ package io.github.xenocider.AgarIO;
 import io.github.xenocider.AgarIO.SexyStuff.Display;
 import io.github.xenocider.AgarIO.entity.Food;
 import io.github.xenocider.AgarIO.entity.PlayerBlobs;
+import io.github.xenocider.AgarIO.listener.KeyListener;
 
 public class AgarIO {
 
@@ -14,6 +15,7 @@ public class AgarIO {
     private static void startGame() {
 
         Display.createDisplay();
+        Display.frame.addKeyListener(new KeyListener());
         System.out.println(new PlayerBlobs().getSkin());
         System.out.println(new Food().getSkin());
 
