820cdfd4c9e86093bfeebde2cce3164b60d2acc7
Izerpizer <izerpizer@gmail.com>
2015-09-28 13:12:15 -0700

    bugah

--- src/io/github/xenocider/AgarIO/Display.java
+++ src/io/github/xenocider/AgarIO/Display.java
@@ -1,12 +1,11 @@
 package io.github.xenocider.AgarIO;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Display{
 
     private static final int WIDTH = 1000;
-    private static final int HEIGHT = 10000;
+    private static final int HEIGHT = 1000;
 
     public static void main(String args[]){
 
@@ -17,10 +16,10 @@ public class Display{
     private static void createDisplay(){
 
         JFrame frame = new JFrame("Agar.io");
+
         frame.setSize(WIDTH, HEIGHT);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-
 
     }
 }
